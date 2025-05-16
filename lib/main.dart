7b12c127a81c2d766828e4af88c1aff881af2abb
Cinta <cjose@orthofx.com>
2025-05-16 15:43:45 +0530

    common ThemeData set for color, appBar, card, btn and text

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:expense_tracker/widgets/expenses_dashboard.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 54, 215, 180)); //creates a set of tonal palettes based on the provided seed color
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,6 +14,31 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer, //a darker shade of the seed color
+          foregroundColor: kColorScheme.primaryContainer, //a lighter shade of the seed color
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: kColorScheme.onSecondaryContainer,
+            fontSize: 16,
+          ),
+        ),
+      ),
       home: ExpensesDashboard(),
     );
   }
